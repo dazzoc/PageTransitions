d5e0c2b39ec3ce83a1653a1da61c5b7e56b1664b
Christopher Dazzo <dazzo.chris@gmail.com>
2022-05-10 09:51:52 -0500

    leave & enter animations

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
+const tlLeave = gsap.timeline({
+    defaults: {duration: 0.75, ease: "Power2.easeOut"}
+});
+
+const tlEnter = gsap.timeline({
+    defaults: {duration: 0.75, ease: "Power2.easeOut"}
+});
+
+// Make the funtions for the leave and enter animations
+const leaveAnimation = (current, done) => {
+    const product = current.querySelector('.image-container');
+    let text = current.querySelector('.showcase-text');
+    const circles = current.querySelectorAll('.circle');
+    const arrow = current.querySelector('.showcase-arrow');
+    return (
+        tlLeave.fromTo(arrow, {opacity: 1, y: 0}, {opacity: 0, y: 50}),
+        tlLeave.fromTo(product, {opacity: 1, y: 0}, {opacity: 0, y: 100, onComplete: done}, '<'),
+        tlLeave.fromTo(text, {opacity: 1, y: 0}, {opacity: 0, y: 100}, '<'), 
+        tlLeave.fromTo(circles, {opacity: 1, y: 0}, {opacity: 0, y: -200, stagger: 0.15, ease: 'back.out(1.7)', duration: 1}, '<')
+    )
+};
+
+const enterAnimation = (current, done) => {
+    const product = current.querySelector('.image-container');
+    let text = current.querySelector('.showcase-text');
+    const circles = current.querySelectorAll('.circle');
+    const arrow = current.querySelector('.showcase-arrow');
+    return (
+        tlLeave.fromTo(arrow, {opacity: 0, y: 50}, {opacity: 1, y: 0}),
+        tlLeave.fromTo(product, {opacity: 0, y: -100}, {opacity: 1, y: 0, onComplete: done}, '<'),
+        tlLeave.fromTo(text, {opacity: 0, y: 100}, {opacity: 1, y: 0}, '<'), 
+        tlLeave.fromTo(circles, {opacity: 0, y: -200}, {opacity: 1, y: 0, stagger: 0.15, ease: 'back.out(1.7)', duration: 1}, '<')
+    )
+};
+
 //run animations
 barba.init({
     preventRunning: true,
@@ -7,13 +42,14 @@ barba.init({
             leave(data){
                 const done = this.async();
                 let current = data.current.container;
-                gsap.fromTo(current, {opacity: 1}, {opacity: 0, duration: 1, onComplete: done});
+                leaveAnimation(current, done);
             },
             enter(data){
                 const done = this.async();
                 let next = data.next.container;
-                gsap.fromTo(next, {opacity: 0}, {opacity: 1, duration: 1, onComplete: done});
+                enterAnimation(next, done);
             },
         },
     ],
 });
+
